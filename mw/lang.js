f1b245f52163cee45a45ed7073db6e29b65e5604
maxkoryukov <maxkoryukov@yandex.ru>
2016-03-24 14:06:31 +0500

    UPG: i18n for routing

---
+++ mw/lang.js
@@ -0,0 +1,36 @@
+"use strict";
+
+var express         = require('express');
+var router          = express.Router();
+
+var debug           = require('debug')('tndr:mw.lang');
+
+var default_language = 'en';
+var default_culture = 'AU';
+
+router.use(function(req, res, next) {
+	debug('=================================================================');
+	debug('LOCALE SETTINGS:', req.lang);
+	next();
+	return;
+});
+
+router.parseCulture = function parseCulture(lang, cult){
+	var l = {
+		lang : lang,
+		cult : cult,
+	};
+
+	l.elang = l.lang || default_language;
+
+	if (l.cult){
+		l.cult = /[a-z]{2}/i.exec(l.cult)[0];
+		l.ecult = l.cult;
+	} else {
+		l.ecult = default_culture;
+	}
+
+	return l;
+}
+
+module.exports = router;
