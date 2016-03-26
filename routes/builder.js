b48c886cd3b097a776a107cd7062165e0a47d252
maxkoryukov <maxkoryukov@yandex.ru>
2016-03-26 15:23:43 +0500

    UPG: builders view (see #5)
    View list of builders, splitted by category

--- routes/builder.js
+++ routes/builder.js
@@ -14,8 +14,28 @@ var baseurl = '/builder';
 router.route(`${baseurl}/`)
 
 	.get(function(req, res, next) {
+		let db = req.app.models;
 
-		res.render('builder/index');
+		db.builder_category.findAll({
+			raw: false,
+			//attributes : ['builder_category', 'name', 'code', 'tip_usage'],
+			include: [db.builder],
+			order: ['sort']
+		})
+			.then(categories => {
+
+				let cs = _.chain(categories)
+					.forEach(x => { if (x.builder) x.builder = x.builder.get(); })
+					.map(_.partial(_.pick, _, ['builder_category', 'name', 'code', 'tip_usage', 'tip_price', 'note', 'builders']))
+					.value();
+
+debug('----------------------------------------------------------------');
+debug(cs);
+				res.render('builder/index', {categories : cs});
+			})
+			.catch(err=> {
+				next(err);
+			});
 	});
 
 module.exports = router;
