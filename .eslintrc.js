358bf4a9156b96b97e268de5ae1a4ac366b17652
maxkoryukov <maxkoryukov@yandex.ru>
2016-07-15 16:38:32 +0500

    volebonet eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,11 @@
 exports = module.exports = {
 	"extends": [
-		"eslint:recommended"
-		//"./node_modules/coding-standard/eslintDefaults.js",
-		// Override eslintDefaults.js
-		//"./node_modules/coding-standard/.eslintrc-es6",
-		// Override .eslintrc-es6
-		//"./node_modules/coding-standard/.eslintrc-jsx",
+		"@volebonet/volebonet"
 	],
 
 	"env": {
 		"node": true,
 		"es6": true,
 	},
-
-	"rules": {
-		// Override any settings from the "parent" configuration
-		"comma-dangle" : ["error", "only-multiline"],
-		"no-unused-vars" : ["warn", { "argsIgnorePattern": "next" }],
-		"curly" : ["error"],
-		"eqeqeq": ["error"],
-		"no-process-exit": ["error"],
-		"no-path-concat": ["error"],
-		"no-extra-parens": ["error", "all", { "nestedBinaryExpressions": false }],
-		"no-cond-assign": ["error"],
-		"strict": ["error", "global"],
-
-		/* CODE STYLE */
-		"indent": ["warn", "tab"],
-	},
-
 	"quiet": true,
 }
